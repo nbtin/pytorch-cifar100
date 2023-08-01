9c258d1fd17a811bdf39caba7798f8ac8fee2cee
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2023-08-01 22:29:58 +0700

    update test

--- test.py
+++ test.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
     parser.add_argument('-weights', type=str, required=True, help='the weights file you want to test')
     parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
     parser.add_argument('-b', type=int, default=16, help='batch size for dataloader')
+    parser.add_argument('-prune', action='store_true', default=False, help='Prune or not')
     args = parser.parse_args()
 
     net = get_network(args)
@@ -37,9 +38,11 @@ if __name__ == '__main__':
         num_workers=4,
         batch_size=args.b,
     )
-
-    net.load_state_dict(torch.load(args.weights))
-    print(net)
+    if args.prune == True:
+        net = torch.load(args.weights)
+    else:
+        net.load_state_dict(torch.load(args.weights))
+        
     net.eval()
 
     correct_1 = 0.0
